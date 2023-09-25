7a8e1b2c8614cb474fd378cd2c6020b577303787
Shreya Venkatapathy <130058451+Shreya-0309@users.noreply.github.com>
2023-09-25 15:08:05 +0530

    Create String_to_integer(atoi).cpp

---
+++ C++/8. String to Integer (atoi)/String_to_integer(atoi).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        double num = 0;
+        int i=0;
+        while(s[i] == ' ') i++;
+        bool pos = s[i] == '+';
+        bool neg = s[i] == '-';
+        pos == true ? i++ : i;
+        neg == true ? i++ : i;
+        while(i < s.length() && s[i] >= '0' && s[i] <= '9'){
+            num = num*10 + (s[i]-'0');
+            i++;
+        }
+        num = neg ? -num : num;
+        num = (num > INT_MAX) ? INT_MAX : num;
+        num = (num < INT_MIN) ? INT_MIN : num;
+        return int(num);
+    }
+};
