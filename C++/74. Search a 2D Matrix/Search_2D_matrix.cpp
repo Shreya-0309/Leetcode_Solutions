681651ca5cbdf13a10eaa2b652a7e84352969303
Shreya Venkatapathy <130058451+Shreya-0309@users.noreply.github.com>
2024-01-07 08:44:17 +0530

    Create Search_2D_matrix.cpp

---
+++ C++/74. Search a 2D Matrix/Search_2D_matrix.cpp
@@ -0,0 +1,23 @@
+// Time complexity: log(m * n)
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int l = 0, r = m * n - 1;
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            int midValue = matrix[mid / n][mid % n];
+            if(midValue == target) {
+                return true;
+            } 
+            else if (midValue < target) {
+                l = mid + 1;
+            } 
+            else {
+                r = mid - 1;
+            }
+        }
+        return false;
+    }
+};
